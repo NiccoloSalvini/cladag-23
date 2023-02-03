4c70858ea49dd199d915d74109e4ddacc7d386bb
Niccolò Salvini <niccolo.salvini27@gmail.com>
2023-02-03 19:21:40 +0100

    documented

--- notebooks/01_load_data.R
+++ notebooks/01_load_data.R
@@ -4,11 +4,15 @@ library(purrr)
 library(vroom)
 library(stringr)
 
+
+## https://dati.anticorruzione.it/opendata/download/dataset/aggiudicazioni/filesystem/aggiudicazioni_csv.zip
 aggiudicazioni = vroom("data/aggiudicazioni_csv.csv", .name_repair = janitor::make_clean_names) # 1,622,864 × 18
+
+## https://dati.anticorruzione.it/opendata/download/dataset/quadro-economico/filesystem/quadro-economico_csv.zip
 quadro_economico = vroom("data/quadro-economico_csv.csv", .name_repair = janitor::make_clean_names) # A tibble: 2,109,476 × 12
 
+## loop in the folder for each monthly file
 cigs_2020 = map_df(list.files("data/2020", full.names = T), ~vroom(.x, .name_repair = janitor::make_clean_names))
-
 
 
 ## A tibble: 417,716 × 98
